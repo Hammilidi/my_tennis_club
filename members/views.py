8d5869031ac6b26a4015fd8d9ffbcd2ea61c203f
Fidelis <yonlifidelis2@gmail.com>
2023-04-17 18:48:43 +0000

    first commit

---
+++ members/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def members(request):
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
+
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'x': ['Apple', 'Banana', 'Cherry'], 
+    'y': ['Apple', 'Banana', 'Cherry'], 
+  }
+  return HttpResponse(template.render(context, request))  
